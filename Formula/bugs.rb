f792f173b622b638ca2fb49c203c5af22293d964
Doug Turnbull <softwaredoug@gmail.com>
2023-08-01 17:44:23 -0400

    Release 0.5.2

--- Formula/bugs.rb
+++ Formula/bugs.rb
@@ -1,8 +1,8 @@
 class Bugs < Formula
   desc "Opinionated tool for doing project management in Jira at Command Line"
-  version "0.5.1"
+  version "0.5.2"
   homepage "https://github.com/reddit/bugs"
-  url "https://github.com/reddit/bugs.git", :tag => "v0.5.1"
+  url "https://github.com/reddit/bugs.git", :tag => "v0.5.2"
   sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
 
   depends_on "ankitpokhrel/jira-cli/jira-cli"
